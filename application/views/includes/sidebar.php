d1855656d90d846119d689ac8e7263972a867f02
Ari Susanto <risusanto@students.ilkom.unsri.ac.id>
2017-11-18 11:39:37 +0700

    selesai

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -48,6 +48,66 @@ if ($this->session->userdata('level') == 1) { ?>
                         </li>
                     </ul>
                 </li>
+                <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-file-o"></i> Assessment</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/assessment">
+                                <i class="fa fa-clone"></i> List Data
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/assessment/list_field">
+                                <i class="fa fa-list-alt"></i> Import Data Excel
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-file-o"></i> Diklat</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/diklat">
+                                <i class="fa fa-clone"></i> List Data
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/diklat/list_field">
+                                <i class="fa fa-list-alt"></i> Import Data Excel
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-file-o"></i> Fit & Proper Test</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/fit_and_proper">
+                                <i class="fa fa-clone"></i> List Data
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/fit_and_proper/list_field">
+                                <i class="fa fa-list-alt"></i> Import Data Excel
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+                <li class="nav-item nav-dropdown">
+                    <a class="nav-link nav-dropdown-toggle" href="#"><i class="fa fa-file-o"></i> Penugasan</a>
+                    <ul class="nav-dropdown-items">
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/penugasan">
+                                <i class="fa fa-clone"></i> List Data
+                            </a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="<?= base_url() ?>index.php/penugasan/list_field">
+                                <i class="fa fa-list-alt"></i> Import Data Excel
+                            </a>
+                        </li>
+                    </ul>
+                </li>
             </li>
             <li class="nav-title">
                 Account Menu
